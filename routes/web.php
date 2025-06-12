749292c0c008225d12d041ebf6c74de7a3cc2e4b
agrz4 <agramahardika479@gmail.com>
2025-06-12 14:32:24 +0700

    feat(route): add dynamic route for news detail by slug

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,4 @@ use App\Http\Controllers\LandingController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [LandingController::class, 'index'])->name('landing');
+Route::get('/news/{slug}', [NewsController::class, 'show'])->name('news.show');
