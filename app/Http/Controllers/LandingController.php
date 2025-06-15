aa2c13900bab17bdd25543dfbbb7a090658a5983
agrz4 <agramahardika479@gmail.com>
2025-06-15 20:32:24 +0700

    fix(view): fix typo in navbar include directive

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Banner;
+use App\Models\News;
+use App\Models\Author;
 use Illuminate\Http\Request;
 
 class LandingController extends Controller
@@ -13,6 +16,6 @@ class LandingController extends Controller
         $news = News::orderBy('created_at', 'desc')->get()->take(4);
         $authors = Author::all()->take(5);
 
-        return view('pages.landing', compact('banners', 'featureds', 'news', 'authors'));
+        return view('pages.landing', compact('banners', 'featured', 'news', 'authors'));
     }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,23 +1,22 @@
 <!doctype html>
-<html>
+<html lang="id">
 
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>@yield('title)</title>
+  <title>@yield('title')</title>
   <link href="{{ asset('assets/css/output.css') }}" rel="stylesheet">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
 </head>
 
 <body>
     <div class="w-full">
-        @include('include.navbar')
+        @include('includes.navbar')
         @yield('content')
-</div>
-
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
-    <script src="/src/js/swiper.js"></script>
+    <script src="{{ asset('src/js/swiper.js') }}"></script>
 </body>
 
 </html>
